171612e52c3a7d195d40d4f657f28b790e085cce
Albo Vieira <albo.vieira@basis.com.br>
2016-06-22 23:59:07 -0300

    Testing.

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
 
 define( 'API_ACCESS_KEY', 'AIzaSyASlWsBkfJ8sX_uPQbAnYGlq32-uztwCbU');
+//define( 'API_SEND_URL', 'https://gcm-http.googleapis.com/gcm/send' );
 define( 'API_SEND_URL', 'https://android.googleapis.com/gcm/send' );
--- src/albov/App/PushNotification.php
+++ src/albov/App/PushNotification.php
@@ -14,7 +14,7 @@ use GuzzleHttp\Psr7\Request;
 class PushNotification
 {
 
-    private $devideId;
+    private $token;
     private $msg;
     private $client;
 
@@ -29,60 +29,26 @@ class PushNotification
 
     public function push(){
 
+        var_dump(self::headers());
+        var_dump(json_encode($this->fields()));
         $response = $this->client->post(API_SEND_URL,[
             'headers' => self::headers(),
-            'data' => $this->fields()
+            'json' => json_encode($this->fields())
         ]);
-
-
-        /*
-        die();
-
-        $request = new Request(
-            'POST',
-            API_SEND_URL,
-            self::headers(),
-            ['json' => $this->fields()]
-        );
-       // var_dump($request);die;
-
-        $response = $this->client->send($request);
-       // var_dump($response);die;
-        /*
-                $response = $this->client->post(API_SEND_URL,[
-                    'headers' => self::headers(),
-                    'json'    => $this->fields(),
-                ]);
-
-                var_dump($response->getStatusCode());
-                var_dump($response->getBody());
-
-        */
-
-       /* $ch = curl_init();
-        curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
-        curl_setopt( $ch,CURLOPT_POST, true );
-        curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
-        curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
-        curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
-        curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
-        $result = curl_exec($ch );
-        curl_close( $ch );
-        echo $result;*/
 
     }
 
     public static function headers(){
         return [
-            'Authorization:key='.API_ACCESS_KEY,
-            'Content-Type:application/json'
+            'Authorization:key= '.API_ACCESS_KEY,
+            'Content-Type: application/json'
         ];
     }
 
     public function fields(){
         return [
-            'registration_ids' 	=> ['3508CF525A2C074D'],
-            'data'			=> Message::mockMsg()
+            'data'			=> ['message' => 'Teste'],
+            'registration_ids' 	=> ['3508CF525A2C074D']
         ];
     }
 
