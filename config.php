78ecd4cca6bb3569166b4bad5fa602156749234c
Albo Vieira <albovieira@gmail.com>
2016-06-27 22:35:08 -0300

    -- Updated branch master

--- config.php
+++ config.php
@@ -1,11 +1,8 @@
 <?php
 /** Google */
-define('KEY_GOOGLE', 'AIzaSyA_l5CRTJU8s6bPS2yTBd6fDXabVyiUavs');
+define('KEY_GOOGLE', 'YOUR_KEY');
 define('GCM_URL', 'https://android.googleapis.com/gcm/send');
 
 define('APNS_URL', 'gateway.sandbox.push.apple.com:2195');
-define('CERTIFICATE_IOS', '../ios_push_certificates/push_cert_dev.pem');
-define('APNS_PASSPHRASE', 'mL32016dev');
-
-
-//coelvf6GOFY:APA91bHg0K7fr5kjNxKNSEecltaYQWnpuXnRT81vIaQyr4j2u9A_MsKIG9EpgQe_9EvIUyHB0zQiiNIkm1RDaimFf-VX0uxKpV3yaph1P9_uq9oel1nzC-K5v6CvHt7-4mAHx6x2425z
+define('CERTIFICATE_IOS', 'YOUR_PUS_CERTIFICATE');
+define('APNS_PASSPHRASE', 'YOUR_PASSPHRASE');
