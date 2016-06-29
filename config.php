551b5d892d6a90dbad019e1741a25a7ecfb28d57
Albo Vieira <albovieira@gmail.com>
2016-06-28 21:08:23 -0300

    -- Refactoring.

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ define('KEY_GOOGLE', 'YOUR_KEY');
 define('GCM_URL', 'https://android.googleapis.com/gcm/send');
 
 define('APNS_URL', 'gateway.sandbox.push.apple.com:2195');
-define('CERTIFICATE_IOS', 'YOUR_PUS_CERTIFICATE');
+define('CERTIFICATE_APNS', 'YOUR_PUS_CERTIFICATE');
 define('APNS_PASSPHRASE', 'YOUR_PASSPHRASE');
