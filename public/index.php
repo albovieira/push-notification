7950c6a5bd4cfbfe0f57df5d3678f765667e6b2e
Albo Vieira <albovieira@gmail.com>
2016-07-26 21:55:21 -0300

    -- Refactoring.

--- public/index.php
+++ public/index.php
@@ -2,15 +2,15 @@
 require_once "../bootstrap.php";
 require_once "../config.php";
 
-$android = \PushNotification\PushFactory::getInstance('android');
+$android = \PushNotification\PushFactory::create('android');
 $android->addToken('tkeb');
 
-$ios = PushNotification\PushFactory::getInstance('ios')
+$ios = PushNotification\PushFactory::create('ios')
     ->withTokens(['e0ba1fb9baf32dff805d30bdba786b8627182dc9aa0772082082ca05d168902d'])
     ->withNotification(['alert' => 'Texto', 'badge' => 1, 'sound' => 'default'])
     ->send();
 
-$android = PushNotification\PushFactory::getInstance('android')
+$android = PushNotification\PushFactory::create('android')
     ->withTokens([
         'coelvf6GOFY:APA91bHg0K7fr5kjNxKNSEecltaYQWnpuXnRT81vIaQyr4j2u9A_MsKIG9EpgQe_9EvIUyHB0zQiiNIkm1RDaimFf-VX0uxKpV3yaph1P9_uq9oel1nzC-K5v6CvHt7-4mAHx6x2425z',
     ])
