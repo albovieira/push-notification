755a9c1ed2457872522f5c43e9c4d69c58db64f1
Albo Vieira <albovieira@gmail.com>
2016-08-25 11:37:12 -0300

    - Changed the way to get the config.

---
+++ push_config.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+   'android' => [
+        'GOOGLE_KEY' => 'YOUR_GOOGLE_KEY',
+        'GCM_URL' => 'https://android.googleapis.com/gcm/send',
+    ],
+    'ios' => [
+        'APNS_URL' => 'gateway.sandbox.push.apple.com:2195',
+        'CERTIFICATE_APNS' => 'PATH_TO_YOUR_CERTIFICATE',
+        'APNS_PASSPHRASE' => 'YOUR_PASSPHRASE',
+    ]
+);
